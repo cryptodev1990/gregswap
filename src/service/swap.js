92fdeffe55c2529dbf0605c7504d9637808b3d68
abelpeterson.dream <abelpeterson.dream@gmail.com>
2022-12-18 13:21:39 -0500

    a

--- src/service/swap.js
+++ src/service/swap.js
@@ -7,7 +7,7 @@ export const createSwap = async (provider, data)=>{
     const signer = provider.getSigner();
     const tokenContract = new ethers.Contract(swapAddress, abi, signer);
     
-    const result = await tokenContract.createSwap(tokenA.address, tokenB.address, amountA, amountB, duration)
+    const result = await tokenContract.createSwap(tokenA.address, tokenB.address, ethers.BigNumber.from(amountA), ethers.BigNumber.from(amountB), ethers.BigNumber.from(duration))
     const aa = result.wait();
     tokenContract.on("CreatedSwap", (tokenA, tokenB, creator, 
         swapAddress, amountA, amountB, 
@@ -25,8 +25,8 @@ export const join = async (provider, swapId) =>{
     const swapAddress = process.env.REACT_APP_SWAP_ADDRESS;
     const signer = provider.getSigner();
     const tokenContract = new ethers.Contract(swapAddress, abi, signer);
-    
-    const result = await tokenContract.join(swapId)
+    const id = ethers.BigNumber.from(swapId);
+    const result = await tokenContract.join(id)
     const aa = result.wait();
     tokenContract.on("Joined", (swapId) => {
         console.log("joined SwapId:", swapId);
@@ -45,7 +45,7 @@ export const withdraw = async (provider, swapId) =>{
     const aa = result.wait();
     tokenContract.on("Withdrawed", (addr, swapId) => {
         console.log("withdrawed:", addr, swapId);
-        console.log(Number(addr, swapId))
+        console.log(Number(swapId))
         alert("withdrawed swapId is ",Number(swapId).toString())
     });
     console.log(aa)
